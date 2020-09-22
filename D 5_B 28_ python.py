76269abd38e256dfc876f378814a052f0fa9e099
musthafashaik126 <71480538+musthafashaik126@users.noreply.github.com>
2020-09-22 10:37:40 +0530

    Day 5 _B 28 python

---
+++ D 5_B 28_ python.py
@@ -0,0 +1,25 @@
+mystudents = ["musthafa","shafi","moosa","pallabi","bhavana","arun","imran","sharukh"]
+print(mystudents)
+#sorted method
+print(sorted(mystudents))
+print(mystudents)
+#sort method
+print(mystudents.sort())
+print(mystudents)
+#reverse method
+print(mystudents.reverse())
+print(mystudents)
+print(mystudents)
+#length
+print(len(mystudents))
+print(mystudents)
+#slicing method
+print(mystudents[0:1])
+print(mystudents[2:4])
+print(mystudents[6:8])
+print(mystudents[0:7:2])
+
+
+
+
+
