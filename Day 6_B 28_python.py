8b6b631327deff28016a751af498d402da4867ba
musthafashaik126 <71480538+musthafashaik126@users.noreply.github.com>
2020-09-23 16:20:54 +0530

    Day_6-B 28

---
+++ Day 6_B 28_python.py
@@ -0,0 +1,14 @@
+location = ["nellore","bangalore","hyderabad","allur","kavali","delhi","iskapalli"]
+print(location)
+print(f"keep up the good work,{location[0].title()}")
+print(f"keep up the good work,{location[6].title()}")
+for lc in location:
+    print(lc)
+
+for z in location:
+    print(f"keep up the good work,{z.title()}")
+    print(f"do send me the practice files of today,{z.title()}.")
+
+for  x in location:
+        print(f"keep up the good work,{x.title()}.")
+        print(f"do send me the practical files of today,{x.title()}.\n")
