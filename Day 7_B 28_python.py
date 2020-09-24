36cb6ae1dc5a439b590967beedaab15ff1899c08
musthafashaik126 <71480538+musthafashaik126@users.noreply.github.com>
2020-09-24 14:40:24 +0530

    Day 7_B 28 _python

---
+++ Day 7_B 28_python.py
@@ -0,0 +1,20 @@
+age = 22
+if age >= 15:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote next year")
+year = 20
+if year >= 55:
+    print("you are eligible to i cet exam ")
+else:
+    print("you are not eligible to i cet exam")
+age = 1
+if age == 1:
+    print("baby will be able to crowl")
+
+elif age == 2:
+    print("baby will be able to stand")
+elif age == 3:
+    print("baby will be able to walk")
+else:
+    print("baby will be able to run")
