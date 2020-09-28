174391e473516e38808df9d4afa517fc2236ae19
musthafashaik126 <71480538+musthafashaik126@users.noreply.github.com>
2020-09-28 14:29:33 +0530

    Day_8_B 28_python

---
+++ Day 8_ B28 _python.py
@@ -0,0 +1,15 @@
+name =(200,50)
+print(name)
+type(name)
+print(type(name))
+students = (1,2,3,4,)
+for x in students:
+    print(x)
+print("Nellore")
+list = [10,20,30]
+print(x)
+print("End of programme")
+
+
+
+
