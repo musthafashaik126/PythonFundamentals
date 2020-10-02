dd3690ec80f8d6dde728f024fd8e9c7081d1e587
musthafashaik126 <71480538+musthafashaik126@users.noreply.github.com>
2020-10-02 07:25:49 +0530

    Day_9_B 28 python

---
+++ Day_9 _ B28  python.py
@@ -0,0 +1,10 @@
+name = {"location":"bangalore","emailid":"musthafashaik126","firstname":"shafi","d  o  b":"05_09_199"}
+print(name)
+print (type(name))
+print(name["emailid"])
+name ["phonenum"] = "7287037329"
+print(name)
+name ["firstname"] = "musthafa"
+print(name)
+del name["emailid"]
+print(name)
